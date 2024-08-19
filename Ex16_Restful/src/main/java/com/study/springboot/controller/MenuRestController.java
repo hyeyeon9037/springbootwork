a74348471833469f280de4e6e4e6d10d278bb540
hyeyeon9037 <hyeyeon9037@naver.com>
2024-08-19 15:11:09 +0900

    20240819

--- Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
+++ Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
@@ -9,28 +9,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.springboot.domain.Menu;
+import com.study.springboot.domain.Taste;
 import com.study.springboot.domain.Type;
 import com.study.springboot.service.MenuService;
 
 @RestController
 @RequestMapping("/menu")
 public class MenuRestController {
-	
-	@Autowired
-	MenuService menuService;
-	
-	@GetMapping
-	public List<Menu> menuAllList() {
-		return menuService.menuAllList();
-	}
-	
-	/*
-	 /menu/type/KR
-	 /menu/type/CH
-	 /menu/type/JP
-	 */
-	@GetMapping("/type/{type}")
-	public List<Menu> findByType(@PathVariable(name="type") Type type) {
-		return menuService.findByType(type);
-	}
+   @Autowired
+   MenuService menuService;
+   
+   @GetMapping
+   public List<Menu> menuAllList(){
+      return menuService.menuAllList();
+      
+   }
+   
+   /*
+     /menu/type/KR
+     /menu/type/CH
+     /menu/type/JP
+    */
+   
+   @GetMapping("/type/{type}")
+   public List<Menu> findByType(@PathVariable(name="type") Type type) {
+      return menuService.findByType(type);
+   }
+   
+   
+   @GetMapping("/type/{type}/taste/{taste}")
+   public List<Menu> findByTypeAndTaste(@PathVariable(name="type") Type type,
+                                  @PathVariable(name="taste") Taste taste)
+    {
+      return menuService.findByAndTaste(type, taste);
+    }
+   
+   
+   
+   
+   
+   
 }
--- Ex16_Restful/src/main/java/com/study/springboot/service/MenuService.java
+++ Ex16_Restful/src/main/java/com/study/springboot/service/MenuService.java
@@ -6,21 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.study.springboot.domain.Menu;
+import com.study.springboot.domain.Taste;
 import com.study.springboot.domain.Type;
 import com.study.springboot.repository.MenuRepository;
 
 @Service
 public class MenuService {
-	
-	@Autowired
-	MenuRepository menuRepository;
+   
+   @Autowired
+   MenuRepository menuRepository;
 
-	public List<Menu> menuAllList() {
-		return menuRepository.findAll();
-	}
+   public List<Menu> menuAllList() {
+      return menuRepository.findAll();
+   }
 
-	public List<Menu> findByType(Type type) {
-		return menuRepository.findByType(type);
-	}
+   public List<Menu> findByType(Type type) {
+      return menuRepository.findByType(type);
+   }
+
+public List<Menu> findByAndTaste(Type type, Taste taste) {
+   
+   return menuRepository.findByAndTaste(type, taste);
+}
 
 }
